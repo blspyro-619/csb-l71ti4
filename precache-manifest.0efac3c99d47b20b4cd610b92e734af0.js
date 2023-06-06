2c56a15e61006db6f07a18886fa60aedcb4880c2
Code Sandbox <hello@codesandbox.io>
2023-06-06 03:04:40 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.0efac3c99d47b20b4cd610b92e734af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538b8ef89f9b16f0a4f98ede05a134d5",
+    "url": "https://blspyro-619.github.io/csb-l71ti4/index.html"
+  },
+  {
+    "revision": "386343beaba5a57c8f24",
+    "url": "https://blspyro-619.github.io/csb-l71ti4/static/css/2.b2ea0959.chunk.css"
+  },
+  {
+    "revision": "b703c8dadc5fd923c69a",
+    "url": "https://blspyro-619.github.io/csb-l71ti4/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "386343beaba5a57c8f24",
+    "url": "https://blspyro-619.github.io/csb-l71ti4/static/js/2.a496073c.chunk.js"
+  },
+  {
+    "revision": "b703c8dadc5fd923c69a",
+    "url": "https://blspyro-619.github.io/csb-l71ti4/static/js/main.09bbac44.chunk.js"
+  },
+  {
+    "revision": "a8a67811be8378d9bb1e",
+    "url": "https://blspyro-619.github.io/csb-l71ti4/static/js/runtime~main.d6bb3d20.js"
+  }
+]);
